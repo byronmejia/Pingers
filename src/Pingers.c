5c91c0c95a20d1fe31e2b01fcb248d8bd487e066
Byron Mejia <byron.mejia@connect.qut.edu.au>
2016-10-09 21:16:29 +1000

    Trialling Jansson

--- src/Pingers.c
+++ src/Pingers.c
@@ -1,8 +1,8 @@
 #include <string.h>
+#include <jansson.h>
 #include <kore/kore.h>
 #include <kore/http.h>
 #include <kore/pgsql.h>
-#include <yajl/yajl_gen.h> 
 
 int init(int);
 int page(struct http_request *);
@@ -37,48 +37,35 @@ int getAllDevices(struct http_request *req) {
   char *name;
   int rows, i;
 
+  kore_log(LOG_NOTICE, json_dumps(json_pack("{}"), JSON_COMPACT));
+
+  /* Start Postres */
+
   req->status = HTTP_STATUS_INTERNAL_ERROR;
 
-	/*
-	 * Initialise our kore_pgsql data structure with the database name
-	 * we want to connect to (note that we registered this earlier with
-	 * kore_pgsql_register()). We also say we will perform a synchronous
-	 * query (KORE_PGSQL_SYNC) and we do not need to pass our http_request
-	 * so we pass NULL instead.
-	 */
   if (!kore_pgsql_query_init(&sql, NULL, "db", KORE_PGSQL_SYNC)) {
     kore_pgsql_logerror(&sql);
     goto out;
   }
 
-	/*
-	 * Now we can fire off the query, once it returns we either have
-	 * a result on which we can operate or an error occured.
-	 */
-	if (!kore_pgsql_query(&sql, "SELECT * FROM device")) {
-		kore_pgsql_logerror(&sql);
-		goto out;
-	}
+  if (!kore_pgsql_query(&sql, "SELECT * FROM device")) {
+    kore_pgsql_logerror(&sql);
+    goto out;
+  }
 
-	/*
-	 * Iterate over the result and dump it to somewhere.
-	 */
-	rows = kore_pgsql_ntuples(&sql);
-	for (i = 0; i < rows; i++) {
-		name = kore_pgsql_getvalue(&sql, i, 0);
-		kore_log(LOG_NOTICE, "name: '%s'", name);
-	}
+  rows = kore_pgsql_ntuples(&sql);
+  for (i = 0; i < rows; i++) {
+    name = kore_pgsql_getvalue(&sql, i, 0);
+    kore_log(LOG_NOTICE, "name: '%s'", name);
+  }
 
-  /* All good. */
   req->status = HTTP_STATUS_OK;
 
   out:
-  http_response(req, req->status, NULL, 0);
-
   kore_pgsql_cleanup(&sql);
 
   char *response = "Hello All Devices";
-  http_response(req, 200, response, strlen(response));
+  http_response(req, req->status, response, strlen(response));
   return (KORE_RESULT_OK);
 }
 
