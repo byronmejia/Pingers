54e50da8f57594efce67cd356f658ad99ec30dd6
Byron Mejia <byron.mejia@connect.qut.edu.au>
2016-10-10 10:21:40 +1000

    snake_betteR

--- src/Pingers.c
+++ src/Pingers.c
@@ -7,13 +7,13 @@
 
 int init(int);
 int page(struct http_request *);
-int getAllDevices(struct http_request *);
-int clearData(struct http_request *);
-int getAllPingsBetween(struct http_request *);
-int getAllPingsOnDate(struct http_request *);
-int getDevicePingsBetween(struct http_request *);
-int getDevicePingsOnDate(struct http_request *);
-int postDevicePing(struct http_request *);
+int get_all_devices(struct http_request *);
+int clear_data(struct http_request *);
+int get_all_pings_between(struct http_request *);
+int get_all_pings_on_date(struct http_request *);
+int get_device_pings_between(struct http_request *);
+int get_device_pings_on_date(struct http_request *);
+int post_device_ping(struct http_request *);
 
 int init(int state) {
   kore_pgsql_register("db", "host=/tmp dbname=pingers");
@@ -27,13 +27,13 @@ int page(struct http_request *req) {
   return (KORE_RESULT_OK);
 }
 
-int clearData(struct http_request *req) {
+int clear_data(struct http_request *req) {
   char *response = "Hello Clear Data";
   http_response(req, 200, response, strlen(response));
   return (KORE_RESULT_OK);
 }
 
-int getAllDevices(struct http_request *req) {
+int get_all_devices(struct http_request *req) {
   char *solution;
 
   json_t *output = device_get_all();
@@ -50,31 +50,31 @@ int getAllDevices(struct http_request *req) {
   return (KORE_RESULT_OK);
 }
 
-int getAllPingsBetween(struct http_request *req) {
+int get_all_pings_between(struct http_request *req) {
   char *response = "Hello All Pings Between";
   http_response(req, 200, response, strlen(response));
   return (KORE_RESULT_OK);
 }
 
-int getAllPingsOnDate(struct http_request *req) {
+int get_all_pings_on_date(struct http_request *req) {
   char *response = "Hello All Pings On Date";
   http_response(req, 200, response, strlen(response));
   return (KORE_RESULT_OK);
 }
 
-int getDevicePingsBetween(struct http_request *req) {
+int get_device_pings_between(struct http_request *req) {
   char *response = "Hello Device Pings Between";
   http_response(req, 200, response, strlen(response));
   return (KORE_RESULT_OK);
 }
 
-int getDevicePingsOnDate(struct http_request *req) {
+int get_device_pings_on_date(struct http_request *req) {
   char *response = "Hello Device Pings on Date";
   http_response(req, 200, response, strlen(response));
   return (KORE_RESULT_OK);
 }
 
-int postDevicePing(struct http_request *req) {
+int post_device_ping(struct http_request *req) {
   char *response = "Hello Post Device Pinga";
   http_response(req, 200, response, strlen(response));
   return (KORE_RESULT_OK);
