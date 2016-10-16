ad153f821b8af86e71df0ffefce8019995890f22
Harris <hjubb@users.noreply.github.com>
2016-10-16 17:47:25 +1000

    clues.sql (#2)
    more extra web scales

--- generate.sql
+++ generate.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS device
 );
 CREATE TABLE IF NOT EXISTS ping
 (
-    time TIMESTAMP NOT NULL,
+    time BIGINT NOT NULL,
     device_id UUID NOT NULL,
     CONSTRAINT ping_time_device_id_pk PRIMARY KEY (time, device_id),
     CONSTRAINT ping_device_id_fk FOREIGN KEY (device_id) REFERENCES device (id)
